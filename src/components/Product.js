2ac280c488c30247660006236a482f938c14f1a3
Fronik13122 <asren67382@gmail.com>
2025-04-08 21:44:59 +0300

    первый коммит

---
+++ src/components/Product.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Product({ name, addToCart }) {
+  return (
+    <li>
+      <p>{name}</p>
+      <button onClick={addToCart}>Добавиь в корзину</button>
+    </li>
+  );
+}
+
+export default Product;
