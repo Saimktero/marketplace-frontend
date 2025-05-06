5528a9da9d47dd64f084a60f1b1de8c80a9dea15
Fronik13122 <asren67382@gmail.com>
2025-05-06 23:59:36 +0300

    добавлена поддержка count и структурированный ответ в getProducts.js

--- src/api/products.js
+++ src/api/products.js
@@ -2,7 +2,19 @@ import axiosInstance from '../axiosInstance';
 
 const API = process.env.REACT_APP_API_BASE_URL;
 
+
+// Получаем список товаров
 export const getProducts = async (page = 1) => {
   const response = await axiosInstance.get(`${API}/products/?page=${page}`);
-  return response.data;
+
+  // Достаём нужные данные из ответа
+  const { count, next, previous, results } = respose.data;
+
+  // Возвращаем красивый объект
+  return {
+    products: results,
+    count: count,
+    nextPage: next,
+    prevPage: previous,
+  };
 };
