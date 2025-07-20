2ca223df950f02ad551fda002ee7f7cd7579c4ab
Fronik13122 <asren67382@gmail.com>
2025-07-20 23:50:40 +0300

    откатил изменения

--- src/api/products.js
+++ src/api/products.js
@@ -7,14 +7,10 @@ const API = process.env.REACT_APP_API_BASE_URL;
 export const getProducts = async (page = 1) => {
   const response = await axiosInstance.get(`${API}/products/?page=${page}`);
 
-  // Достаём нужные данные из ответа
-  const { count, next, previous, results } = response.data;
-
-  // Возвращаем красивый объект
   return {
-    products: results,
-    count: count,
-    nextPage: next,
-    prevPage: previous,
+    results: response.data.results,
+    count: response.data.count,
+    nextPage: response.data.next,
+    prevPage: response.data.previous,
   };
 };
