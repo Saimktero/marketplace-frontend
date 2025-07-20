f4f578bba1066876d8b9a71e90c13dd64a190841
Fronik13122 <asren67382@gmail.com>
2025-07-20 21:25:28 +0300

    защита от undefined

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -5,7 +5,7 @@ function ProductList({ products, addToCart }) {
     <div>
       <ul>
         {products.length > 0 ? (
-          products.map(product => (
+          (products?.results || []).map(product => (
             <Product
              key={product.id}
              name={product.name}
