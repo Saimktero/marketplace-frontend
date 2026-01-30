19bef42615b5f9be823b042eff8363b4b4ee70b3
Saimktero <priluckijkirill30@gmail.com>
2026-01-30 15:13:27 +0300

    Fixed the rendering of the product list (ProductList expects an array)

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,21 +1,17 @@
 import Product from './Product';
 
 function ProductList({ products, addToCart }) {
-  if (!products) return <p>Загрузка товаров...</p>;
+  if (!products || products.length === 0) return <p>Загрузка товаров...</p>;
   return (
     <div>
       <ul>
-        {products?.results?.length > 0 ? (
-          products.results.map(product => (
-            <Product
-              key={product.id}
-              name={product.name}
-              addToCart={() => addToCart(product)}
-            />
-          ))
-        ) : (
-          <p>Загрузка товаров...</p>
-        )}
+           {products.map(product => (
+          <Product
+            key={product.id}
+            name={product.name}
+            addToCart={() => addToCart(product)}
+          />
+        ))}
       </ul>
     </div>
   );
