50d6e0d2b5fcf5933e5f611016f22f32ad4b6e05
Saimktero <priluckijkirill30@gmail.com>
2026-02-05 13:38:25 +0300

    добавлен импорт линка

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,9 @@
+import { Link } from 'react-router-dom';
 export function Home() { 
     return(
     <main>
-        <section>    
+        <section>
+            <h1>Онлайн-маркетплейс anyways</h1>    
             <p>Онлайн-маркетплейс с товарами для дома, работы и повседневной жизни по доступным ценам.</p>
             <Link to='/products' className='btn'>Перейти в каталог</Link>
         </section>
