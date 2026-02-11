e8a7e63ec8ec37852b1e3ddba3486cfc0bc754bd
Saimktero <priluckijkirill30@gmail.com>
2026-02-11 10:03:24 +0300

    откат до предыдущего комитта

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,9 +4,9 @@ function ProductList({ products, addToCart }) {
   if (!products || products.length === 0) return <p>Загрузка товаров...</p>;
 
   return (
-    <ul>
+    <ul className="products-grid">
       {products.map(product => (
-        <li key={product.id}>
+        <li key={product.id} className="product-card">
           <Product
             name={product.name}
             addToCart={() => addToCart(product)}
